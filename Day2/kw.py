bab5c2a5d919f21b0b07b0206814f153b89e1749
A SIVA PRADEEP KUMAR <kum.pradeepyadav143@gmail.com>
2026-05-27 17:48:09 +0530

    Add files via upload

---
+++ Day2/kw.py
@@ -0,0 +1,49 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============================================= RESTART: C:/Users/kumpr/Desktop/Python-course-work/Day2/kw.py =============================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+myvar=10
+myvar
+10
+>>> a=3
+>>> if % 3=0
+SyntaxError: invalid syntax
+>>> a
+3
+>>> a=b=c=d=10
+>>> d
+10
+>>> e
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    e
+NameError: name 'e' is not defined
+>>> Myvar=2
+>>> myvar@=1
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    myvar@=1
+TypeError: unsupported operand type(s) for @=: 'int' and 'int'
+>>> my_var=8
+>>> my var=8
+SyntaxError: invalid syntax
+>>> a,b,c=6
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    a,b,c=6
+TypeError: cannot unpack non-iterable int object
+>>> ab=2
+>>> a
+10
+>>> ab
+2
+>>> var=10
+>>> var
+10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> c
+30
